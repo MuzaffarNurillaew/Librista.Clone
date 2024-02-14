4a130a7c2af876c051e2247b57ba3f189073070c
Muzaffar Nurillayev <nurillaewmuzaffar@gmail.com>
2024-02-14 12:32:16 +0500

    Add Identity.

--- src/Librista.Domain/Entities/BorrowingRecord.cs
+++ src/Librista.Domain/Entities/BorrowingRecord.cs
@@ -1,4 +1,5 @@
 using Librista.Domain.Commons;
+using Microsoft.AspNetCore.Identity;
 
 namespace Librista.Domain.Entities;
 
@@ -18,4 +19,9 @@ public class BorrowingRecord : Auditable
     public decimal? TotalFines { get; set; }
     public bool? IsPaid { get; set; }
     public bool IsReturned { get; set; }
+}
+
+public class User : IdentityUser<long>
+{
+    
 }
